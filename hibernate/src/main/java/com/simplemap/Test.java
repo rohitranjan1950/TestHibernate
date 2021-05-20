963f2778edced85d11c2a44c55d601c8b8b3b851
Rohit Ranjan <Rohit Ranjan@LAPTOP-V8PDAMNS>
2021-05-20 14:46:15 +0530

    Edit Test.java class

--- hibernate/src/main/java/com/simplemap/Test.java
+++ hibernate/src/main/java/com/simplemap/Test.java
@@ -23,13 +23,15 @@ public class Test {
 
 		LocalTime ktime = LocalTime.of(16, 38);
 
-		byte byk = 15;
+		LocalDateTime datetime1 = LocalDateTime.of(kdt, ktime);
 
-		byte byk1 = 56;
-
-		short shk = 62;
-
-		short shk1 = 342;
+//		byte byk = 15;
+//
+//		byte byk1 = 56;
+//
+//		short shk = 62;
+//
+//		short shk1 = 342;
 
 //		Employee emp = new Employee(101, true, byk, shk, 'B', 843188L, 45.9f, 2535.42, "Rohit Ranjan", date, time,
 //				datetime);
@@ -38,6 +40,8 @@ public class Test {
 //				date, time, datetime);
 
 		Employee emp = new Employee(8431L, 45.8f, 642.9, "Rohit Ranjan", date, time, datetime);
+
+		Employee emp1 = new Employee(9342L, 623.92f, 25600.8, "Priyanka Ranjan", kdt, ktime, datetime1);
 
 		Transaction tx = null;
 
@@ -48,13 +52,13 @@ public class Test {
 
 			int id = (int) session.save(emp);
 
-//			int idk = (int) session.save(emp1);
+			int idk = (int) session.save(emp1);
 
 			tx.commit();
 
 			System.out.println("Record Inserted : " + id);
 
-//			System.out.println("Record Inserted : " + idk);
+			System.out.println("Record Inserted : " + idk);
 
 		} catch (HibernateException e) {
 
